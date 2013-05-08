1d31c761cd91888f9c25e6f87ad488b2aba82e43
Andre Goerres <andre.goerres+git@gmail.com>
2013-05-08 10:31:39 +0200

    Added a usage output if no input is given.

--- vhdl_halo_constant.py
+++ vhdl_halo_constant.py
@@ -4,10 +4,18 @@
 # Import stuff
 import sys
 
+def PrintUsage():
+    print 'Usage: ./vhdl_halo_constant <constant_name> <real_value>'
+    print ''
+    print '  <constant_name>: Your constant name.'
+    print '  <real_value>:    The real value of the signal in bit format that should be changed.'
+    print ''
+    print '  Example:   ./vhdl_halo_constant TESTSIG 001'
 
 # The main program
 def main():
     if len(sys.argv) != 3:
+        PrintUsage()
         sys.exit()
 
     constantName = sys.argv[1]
@@ -18,7 +26,7 @@ def main():
         mask = 1 << pos
         haloValue = realValue ^ mask
         haloValueStr = str(bin(haloValue))[2:].zfill(wordLength)
-        print 'constant {}_{} := "{}";'.format(constantName, pos, haloValueStr)
+        print 'constant {}_{} : state_type := "{}";'.format(constantName, pos, haloValueStr)
 
 
 if __name__ == '__main__':
