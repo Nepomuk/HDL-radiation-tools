1c222f9af94e17f030f6d4d04293162ce0e40f23
André Goerres <andre.goerres+git@gmail.com>
2014-02-17 16:53:04 +0100

    Fixes incopatibility with python 2.6

--- vhdl_halo_constant.py
+++ vhdl_halo_constant.py
@@ -26,7 +26,7 @@ def main():
         mask = 1 << pos
         haloValue = realValue ^ mask
         haloValueStr = str(bin(haloValue))[2:].zfill(wordLength)
-        print 'constant {}_{} : state_type := "{}";'.format(constantName, pos, haloValueStr)
+        print 'constant {0}_{1} : state_type := "{2}";'.format(constantName, pos, haloValueStr)
 
 
 if __name__ == '__main__':
